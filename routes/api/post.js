3d37e480113a36b42ba620ee4c9c7de8e04db74e
yousef2002307 <85695400+yousef2002307@users.noreply.github.com>
2026-08-21 14:46:16 +0300

    feat: implement post API routes and initialize application logging

--- routes/api/post.js
+++ routes/api/post.js
@@ -7,4 +7,5 @@ router.post("/post", verifyJWT, PostController.store.bind(PostController));
 router.get("/post/:id", verifyJWT, PostController.show.bind(PostController));
 router.delete("/post/:id", verifyJWT, PostController.delete.bind(PostController));
 router.get("/post", verifyJWT, PostController.index.bind(PostController));
+router.put("/post/:id", verifyJWT, PostController.edit.bind(PostController));
 module.exports = router;
